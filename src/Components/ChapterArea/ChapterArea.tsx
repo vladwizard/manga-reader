5b55adf1caeea347cc79b40ac8796b67c0ca1592
vladwizard <v290801@gmail.com>
2022-12-09 21:33:02 +0300

    -

--- src/Components/ChapterArea/ChapterArea.tsx
+++ src/Components/ChapterArea/ChapterArea.tsx
@@ -1,5 +1,5 @@
 import {getChapterByLanguage, getChapters, getLanguagesOfChapters} from "../../orders";
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import './ChapterArea.css'
 
